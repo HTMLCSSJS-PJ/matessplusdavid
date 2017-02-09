9d4d21569f85a69c3eb1dfa2aff546c67150ddcd
Pierce <piercejord@gmail.com>
2017-02-09 12:09:59 +0000

    95% Complete, Live on www.matessplusdavid.net

--- thankyou.php
+++ thankyou.php
@@ -1,10 +1,63 @@
 <!DOCTYPE html>
 <html>
-<body>
-
-<h1>Thanks You</h1>
-
-<p>From Matesss and David.</p>
-
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <!--[if IE]>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <![endif]-->
+  <title class="title">Thank You</title>
+  <!--REQUIRED STYLE SHEETS-->
+  <!-- BOOTSTRAP CORE STYLE CSS -->
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+  <!-- FONTAWESOME STYLE CSS -->
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <!-- CUSTOM STYLE CSS -->
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <!-- GOOGLE FONT -->
+  <link href='http://fonts.googleapis.com/css?family=Ruluko' rel='stylesheet' type='text/css' />
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+  <![endif]-->
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body class="color-white">
+  <!-- Navigation -->
+  <nav class='navbar navbar-inverse navbar-fixed-top'>
+    <div class='container-fluid'>
+      <header class='navbar-header'>
+        <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>
+          <span class='icon-bar'></span>
+          <span class='icon-bar'></span>
+          <span class='icon-bar'></span>
+        </button>
+        <a class='navbar-brand' href='index.php'><i class="fa fa-home" aria-hidden="true"></i> Matess + David</a>
+      </header>
+      <div class='collapse navbar-collapse' id='myNavbar'>
+        <ul class='nav navbar-nav navbar-right'>
+          <li><a href='#'><i class="fa fa-calendar-check-o" aria-hidden="true"></i> 18 March 2017</a></li>
+          <li><a href='#'><i class="fa fa-clock-o" aria-hidden="true"></i> 14:00</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!--Header section  -->
+  <div class="container" id="home">
+    <div class="row text-center">
+      <div class="col-md-12">
+        <img id="mdg" src="assets/img/04.jpg" alt="Matess and David" height="400" width="350">
+        <h1 style="color:#9C0000;">Thank you for your message,</h1>
+        <p class="lead">Best Wishes, Matess and David.</p>
+        <br><br><br><br>
+        <div class="head-last"></div>
+      </div>
+    </div>
+  </div>
+  <!--End Header section  -->
 </body>
 </html>
